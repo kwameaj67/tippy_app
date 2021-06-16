7bcbcb00d0925ea7357a2041df6bfe4ac7f73cd2
Kwame <kwameagyenimboateng64@gmail.com>
2021-06-16 15:36:05 +0000

    Initial Commit

---
+++ TipCalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculatorApp
+//
+//  Created by Kwame Agyenim - Boateng on 16/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
