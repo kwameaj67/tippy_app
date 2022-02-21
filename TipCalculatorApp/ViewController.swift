ca2e53bcb48b826f8f0a65334e725b920b65d917
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-21 08:14:17 +0000

    implementation of tip logic in VC

--- TipCalculatorApp/ViewController.swift
+++ TipCalculatorApp/ViewController.swift
@@ -9,11 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountLbl: UITextField!
+    @IBOutlet weak var percentageLbl: UITextField!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var tipLbl: UILabel!
+    @IBOutlet weak var totalBillLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calculateButton.layer.cornerRadius = calculateButton.layer.frame.height / 2 
+        title = "WeSplit"
+        handler(message: "Hi, Kwame") { (result) in
+            print("RESULT:\(result)")
+        }
+        
+    }
+    
+    func calculateBill(){
+        let billAmount = Double(amountLbl.text ?? "0")!
+        
+        let percent = Double(percentageLbl.text ?? "") ?? 0.0
+        
+        let percentageAmount = (percent/100)
+        print("\(percentageAmount)")
+        let tipAmount = percentageAmount * billAmount
+        print("\(tipAmount)")
+        
+        tipLbl.text = "Ghc\(tipAmount)"
+        
+        let totalBillAmount = billAmount + tipAmount
+        
+        totalBillLbl.text = "Ghc\(totalBillAmount)"
     }
 
+    @IBAction func calculateTipTap(_ sender: Any) {
+        if amountLbl.text == ""{
+            showAlert(title: "Error!", message: "Please enter your amount you spent")
+        }else if percentageLbl.text == ""{
+            showAlert(title: "Error!", message: "Please enter tip percentage you spent")
+        }else{
+            calculateBill()
+        }
+            
+    }
+    
+    
+    func showAlert(title:String,message:String){
+        let alertView = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("Cancelled")
+            
+        }
+        alertView.addAction(cancel)
+        present(alertView, animated: true, completion: nil)
+        }
+    
+  
 
+    func handler(message:String,completion: (_ result:String) -> Void){
+            print("MESSAGE:\(message)")
+            print("This is the code for completion handler")
+            
+    }
+    
 }
 
